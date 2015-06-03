60c77da285b1634e52a8358e2b04635dcd1e598c
Can ZHANG <zhangcan@letv.com>
2015-06-03 18:34:10 +0800

    WIP: graph for nodes

--- web/app/main.js
+++ web/app/main.js
@@ -1,3 +1,6 @@
+// stub for testing
+var stub = undefined;
+
 var SearchableNodeList = React.createClass({
     componentDidMount: function(){
         $.ajax({
@@ -13,25 +16,33 @@ var SearchableNodeList = React.createClass({
     },
     getInitialState: function () {
         return {node_list: [],
-                keywords: ""}
+                keywords: "",
+                pressed: null}
     },
     handleKeyword: function(keyword){
         this.setState({keywords:keyword})
+    },
+    handleNodeClick: function(node_id){
+        this.setState({pressed:node_id});
     },
     render: function(){
         return (
             <div>
                 <SearchBar onNewKeywords={this.handleKeyword} />
-                <NodeList node_list={this.state.node_list} keywords={this.state.keywords} />
+                <NodeList node_list={this.state.node_list} keywords={this.state.keywords}
+                    onNodeClick={this.handleNodeClick} />
+                <div>
+                    <NodeGraph node_id={this.state.pressed} />
+                </div>
             </div>
         )
     }
-})
+});
 
 var SearchBar = React.createClass({
     handleSearch: function(event){
-        event.preventDefault()
-        var keywords = React.findDOMNode(this.refs.keywords).value.trim()
+        event.preventDefault();
+        var keywords = React.findDOMNode(this.refs.keywords).value.trim();
         this.props.onNewKeywords(keywords)
     },
     render: function(){
@@ -42,12 +53,13 @@ var SearchBar = React.createClass({
             </form>
         )
     }
-})
+});
 
 
 var NodeList = React.createClass({
     render: function() {
-        var key = this.props.keywords
+        var key = this.props.keywords;
+        var onNodeClick = this.props.onNodeClick;
         var nodeList = this.props.node_list.map(function(node, index){
             if (node.name.toLowerCase().indexOf(key) == -1 &&
                 node.ip.indexOf(key) == -1 &&
@@ -59,13 +71,14 @@ var NodeList = React.createClass({
             }
             var tags = node.tags.map(function(tag, index){
                 return(
-                    <span>tag['name']</span>
+                    <span>{tag['name']}</span>
                 )
-            })
+            });
             return(
-                <NodeEntry name={node.name} ip={node.ip} tags={node.tags} key={index} />
+                <NodeEntry name={node.name} ip={node.ip} tags={tags} key={index}
+                    id={node._id} onEntryClick={onNodeClick} />
             )
-        })
+        });
         return (
             <table>
                 <tr>
@@ -78,13 +91,16 @@ var NodeList = React.createClass({
             </table>
         )
     }
-})
+});
 
 
 var NodeEntry = React.createClass({
+    handleClick: function(){
+        this.props.onEntryClick(this.props.id)
+    },
     render: function(){
         return (
-            <tr>
+            <tr onClick={this.handleClick}>
                 <td>{this.props.name}</td>
                 <td>{this.props.ip}</td>
                 <td>{this.props.tags}</td>
@@ -92,9 +108,83 @@ var NodeEntry = React.createClass({
             </tr>
         )
     }
-})
+});
+
+var influxdb_url = 'http://192.169.0.39:8086';
+var q_param = function(q){
+    return {
+        u: 'root',
+        p: 'root',
+        db: 'rebase',
+        q: q
+    }
+};
+
+var NodeGraph = React.createClass({
+    getInitialState: function(){
+        $.ajax({
+            url: influxdb_url + '/query?' + $.param(
+                q_param('SHOW MEASUREMENTS')),
+            dataType: 'json',
+            success: function(data){
+                var measure_list = _.flatten(data.results[0].series[0].values);
+                var measurements = {};
+                measure_list.map(function(m){
+                    measurements[m] = [];
+                });
+                this.setState({measurements: measurements});
+                stub = this.state
+            }.bind(this),
+            error: function(xhr, status, err){
+                console.error('SHOW MEASUREMENTS', status, err.toString())
+            }.bind(this)
+        });
+        return {
+            data: [],
+            node: null,
+            measurements: []
+        }
+    },
+    componentWillReceiveProps: function(nextProps){
+        var id = nextProps.node_id;
+        if(id){
+        $.ajax({
+            url: 'node/' + id,
+            dataType: 'json',
+            success: function(data){
+                this.setState({node:data});
+                $.ajax({
+                    url: influxdb_url + '/query?' + $.param(
+                       q_param("select value from cpu where device='cpu0' and measure='user'")),
+                    dataType: 'json',
+                    success: function(data){
+                        console.log(data)
+                    }.bind(this),
+                    error: function(xhr, status, err){
+                        console.error('Fetching data', status, err.toString())
+                    }
+                })
+            }.bind(this),
+            error: function(xhr, status, err){
+                console.error(id, status, err.toString())
+            }.bind(this)
+        })}
+    },
+    render: function(){
+        return (
+            <div id="graph" style={{width:'300px',height:'200px'}}></div>
+        )
+    },
+    componentDidUpdate: function(prevProps, prevState) {
+        $.plot('#graph', [{
+            data: [[1,2], [2,5], [3,4], [4,6]],
+            lines: {show:true, fill:true},
+            points: {show:true},
+        }]);
+    }
+});
 
 React.render(
     <SearchableNodeList url="nodes" />,
     document.getElementById('content')
-)
+);
