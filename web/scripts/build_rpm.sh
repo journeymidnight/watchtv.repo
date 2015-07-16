795f8d7eb6e577c6e1046e7828dc97f2efb8c713
Can ZHANG <zhangcan@letv.com>
2015-07-16 13:09:46 +0800

    Modify RPM packaging script to copy files before `fpm`

--- web/scripts/build_rpm.sh
+++ web/scripts/build_rpm.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+set -x
+# get version, something like 0.1-3-gf4d86cc
+version=`git describe|cut -c 2-`
 
-fpm -d nodejs -v 0.1 -n watchtv -s dir -t rpm /usr/lib/watchtv /var/log/watchtv/watchtv.log /etc/init.d/watchtv /etc/logrotate.d/watchtv
+# copy files to expected location
+mkdir -p /usr/lib/watchtv
+cp -r ../* /usr/lib/watchtv/
+mkdir -p /var/log/watchtv
+touch /var/log/watchtv/watchtv.log
+cp ./watchtv.sh /etc/init.d/watchtv
+cp ./watchtv.logrotate /etc/logrotate.d/watchtv
+
+fpm -d nodejs -v $version -n watchtv -s dir -t rpm /usr/lib/watchtv /var/log/watchtv/watchtv.log /etc/init.d/watchtv /etc/logrotate.d/watchtv
