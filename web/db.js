8254dd7182c6584c1beea371e96ac8a10e43fdcd
马源英 <mayuanying@letv.com>
2015-08-26 15:51:15 +0800

    db.js

--- web/db.js
+++ web/db.js
@@ -26,6 +26,10 @@ var nodeSchema = new Schema({
         description: String,
         ips: [String],
         tags: [{type: Schema.Types.ObjectId, ref: 'Tag'}],
+        graphInfo: [{
+            dashboard: [{type: Schema.Types.ObjectId, ref: 'Dashboard'}],
+            user: {type: Schema.Types.ObjectId, ref: 'User'}
+        }],
         state: {type: String, enum: states},
         failedRules: [String]
     },
@@ -41,12 +45,7 @@ var Node = mongoose.model('Node', nodeSchema);
 var roles = ['Root', 'User'];
 var userSchema = new Schema({
         name: String,
-        dashboards: [{
-            node: {type: Schema.Types.ObjectId, ref: 'Node'},
-            ip: String,
-            metric: String,  // comma separated metric name, e.g. "ceph,read_Bps"
-            time: String
-        }],
+        dashboard: [{type: Schema.Types.ObjectId, ref: 'Dashboard'}],
         tags: [{type: Schema.Types.ObjectId, ref: 'Tag'}],
         role: {type: String, enum: roles}
     },
@@ -57,10 +56,23 @@ var userSchema = new Schema({
 
 var User = mongoose.model('User', userSchema);
 
+var dashboardSchema = new Schema({
+        ip: String,
+        metric: String,  // comma separated metric name, e.g. "ceph,read_Bps"
+        time: String
+    },
+    {
+        collection: "Dashboard"
+    }
+);
+
+var Dashboard = mongoose.model('Dashboard', dashboardSchema);
+
 mongoose.connect(config.db.mongodbURL);
 
 module.exports = {
     Tag: Tag,
     Node: Node,
-    User: User
+    User: User,
+    Dashboard: Dashboard
 };
