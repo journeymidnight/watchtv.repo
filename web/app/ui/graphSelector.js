782e0bb204418a81ddf2ae6ecce08bb77aa72316
Can ZHANG <zhangcan@letv.com>
2015-10-13 18:03:53 +0800

    Fix a UI bug

--- web/app/ui/graphSelector.js
+++ web/app/ui/graphSelector.js
@@ -10,7 +10,7 @@ var GraphSelector = React.createClass({
     mixins: [mixins.materialMixin],
     getInitialState: function() {
         return {
-            measurements: null,
+            measurements: this.props.measurements,
             select:this.props.selected
         };
     },
