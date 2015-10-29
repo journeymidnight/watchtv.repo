0e4d289d852bb504a25b1b8f7f61d6238b3c34b8
Can ZHANG <zhangcan@letv.com>
2015-10-29 16:32:51 +0800

    Add reset button for dropdown filters

--- web/app/ui/searchBar.js
+++ web/app/ui/searchBar.js
@@ -3,6 +3,8 @@ var Pagination = require('react-bootstrap/lib/Pagination');
 var TextField = require('material-ui/lib/text-field');
 var RaisedButton = require('material-ui/lib/raised-button');
 var DropDownMenu = require('material-ui/lib/drop-down-menu');
+var IconButton = require('material-ui/lib/icon-button');
+var SvgIcon = require('material-ui/lib/svg-icon');
 
 var mixins = require('../mixins.js');
 
@@ -83,6 +85,10 @@ var SearchBar = React.createClass({
     getInitialState: function() {
         var state = {},
             that = this;
+        var doUpdates = function() {
+            that.updateMenuItems();
+            that.raiseStates();
+        };
         this.props.additionalFilter.split(' ').map(function (dropdownName) {
             if (dropdownName === '') return;
 
@@ -96,10 +102,11 @@ var SearchBar = React.createClass({
                     newState['idc'] = '';
                     newState['region'] = '';
                 }
-                var doUpdates = function() {
-                    that.updateMenuItems();
-                    that.raiseStates();
-                };
+                that.setState(newState, doUpdates);
+            };
+            that[dropdownName + 'Reset'] = function() {
+                var newState = {};
+                newState[dropdownName] = '';
                 that.setState(newState, doUpdates);
             };
         });
@@ -161,6 +168,18 @@ var SearchBar = React.createClass({
                                     onChange={that[dropdownName + 'Handler']}
                                     selectedIndex={selectedIndex}
                                     key={dropdownName + 'DropDown'} />);
+            if (selectedIndex !== 0) {
+                searchComponents.push(
+                    <IconButton tooltip="Reset" onClick={that[dropdownName + 'Reset']}>
+                        <SvgIcon hoverColor="#e53935">
+                            <svg fill="#444444" height="20" viewBox="0 0 24 24" width="20" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm5 13.59L15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12 17 15.59z"/>
+                                <path d="M0 0h24v24H0z" fill="none"/>
+                            </svg>
+                        </SvgIcon>
+                    </IconButton>
+                )
+            }
         });
 
         return (
